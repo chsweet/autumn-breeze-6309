1fdccae628b0103d269412e68ff3e568bd283344
Carina Sweet <carina.sweet06@gmail.com>
2021-08-10 11:00:37 -0600

    Refactor airlines method for extention

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,10 +1,11 @@
 class Airline < ApplicationRecord
   has_many :flights
 
-  def airline_adult_passengers
+  def adult_passengers_order_flights
     flights.joins(:passengers)
-    .select('passengers.name as passenger_name')
+    .select('passengers.name as passenger_name, count(flights.id) as flight_count')
     .where('passengers.age >= ?', 18)
     .group('passenger_name')
+    .order(flight_count: :desc)
   end
 end
--- spec/models/airline_spec.rb
+++ spec/models/airline_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe Airline do
     @flight_1 = @airline_1.flights.create!(number: '6837', date: '08/15/2021', departure_city: 'Denver, CO', arrival_city: 'Anchorage, AK')
     @flight_2 = @airline_1.flights.create!(number: '745', date: '10/22/2021', departure_city: 'Denver, CO', arrival_city: 'Atlanta, GA')
     @flight_3 = @airline_1.flights.create!(number: '6837', date: '08/25/2021', departure_city: 'Anchorage, AK', arrival_city: 'Denver, CO')
+    @flight_4 = @airline_1.flights.create!(number: '827', date: '08/25/2021', departure_city: 'Atlanta, GA', arrival_city: 'Denver, CO')
 
     @passenger_1 = Passenger.create!(name: 'Nicki', age: 33)
     @passenger_2 = Passenger.create!(name: 'Chris', age: 38)
@@ -19,20 +20,24 @@ RSpec.describe Airline do
 
     FlightLog.create!(flight: @flight_1, passenger: @passenger_1)
     FlightLog.create!(flight: @flight_1, passenger: @passenger_2)
+    FlightLog.create!(flight: @flight_1, passenger: @passenger_4)
     FlightLog.create!(flight: @flight_2, passenger: @passenger_3)
     FlightLog.create!(flight: @flight_2, passenger: @passenger_4)
+    FlightLog.create!(flight: @flight_2, passenger: @passenger_2)
     FlightLog.create!(flight: @flight_3, passenger: @passenger_1)
     FlightLog.create!(flight: @flight_3, passenger: @passenger_2)
+    FlightLog.create!(flight: @flight_3, passenger: @passenger_4)
+    FlightLog.create!(flight: @flight_4, passenger: @passenger_4)
   end
 
   describe 'instance methods' do
-    describe '#airline_adult_passengers' do
-      it 'returns unique list of adult passengers' do
-        actual = @airline_1.airline_adult_passengers.map do |passenger|
+    describe '#adult_passengers_order_flights' do
+      it 'returns unique list of adult passengers ordered by flight count' do
+        actual = @airline_1.adult_passengers_order_flights.map do |passenger|
           passenger.passenger_name
         end
 
-        expect(actual).to match_array([@passenger_1.name, @passenger_2.name, @passenger_4.name])
+        expect(actual).to eq([@passenger_4.name, @passenger_2.name, @passenger_1.name])
         expect(actual).to_not include(@passenger_3.name)
       end
     end
